5bc1ff67bb6b8babedf297d9042433f2713a8f97
usman-codes <usmanamin31045@gmail.com>
2024-05-28 22:59:21 +0100

    Initial Commit

--- darzi_app/lib/screens/Splashscreen.dart
+++ darzi_app/lib/screens/Splashscreen.dart
@@ -1,20 +1,42 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class splashscreen extends StatelessWidget {
+import 'package:darzi_app/screens/Homescreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class splashscreen extends StatefulWidget {
   const splashscreen({super.key});
+
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.deepPurple,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                "assets/Icon.png",
-                width: 200,
-                height: 300,
+              Animate(
+                effects: [
+                  FadeEffect(delay: 300.ms),
+                ],
+                child: Image.asset(
+                  "assets/Icon2.png",
+                  width: 180,
+                  height: 250,
+                ),
               ),
               const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -22,16 +44,18 @@ class splashscreen extends StatelessWidget {
                   Text(
                     "DARZI",
                     style: TextStyle(
-                        fontSize: 50,
-                        color: Color.fromARGB(255, 63, 20, 163),
-                        fontWeight: FontWeight.bold),
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                  SizedBox(width: 10),
                   Text(
-                    "Khata",
-                    style: TextStyle(fontSize: 50),
+                    "DIGITAL",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   )
                 ],
-              )
+              ).animate(delay: 400.ms).slideY(curve: Curves.easeIn)
             ],
           ),
         ));
