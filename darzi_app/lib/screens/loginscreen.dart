edc4b5309244df5c3746d76a0e291f761249303d
usman-codes <usmanamin31045@gmail.com>
2024-06-04 13:33:35 +0100

    Initial Commit

--- darzi_app/lib/screens/loginscreen.dart
+++ darzi_app/lib/screens/loginscreen.dart
@@ -43,48 +43,101 @@ class _loginscreenState extends State<loginscreen> {
               width: double.infinity,
               child: Padding(
                 padding: const EdgeInsets.only(left: 20, right: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    TextField(
-                      controller: TextEditingController(),
-                      decoration: InputDecoration(
-                          suffixIcon: const Icon(Icons.email_outlined),
-                          label: const Text(
-                            "Email",
-                            style: TextStyle(color: Colors.deepPurple),
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      controller: TextEditingController(),
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          suffixIcon: const Icon(Icons.visibility_off_rounded),
-                          label: const Text(
-                            "Password",
-                            style: TextStyle(color: Colors.deepPurple),
-                          )),
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Container(
-                      height: 40,
-                      width: 170,
-                      decoration: BoxDecoration(
-                          gradient:
-                          LinearGradient(colors: [Colors.deepPurple, Colors.purple]),
-                        borderRadius: BorderRadius.circular(30)
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 30,
                       ),
-                      child: Center(child: Text(
-                          "Sign in",
-                        style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),
-                      )),
-                    )
-                  ],
+                      TextField(
+                        controller: TextEditingController(),
+                        decoration: InputDecoration(
+                            suffixIcon: const Icon(Icons.email_outlined),
+                            label: const Text(
+                              "Email",
+                              style: TextStyle(color: Colors.deepPurple),
+                            )),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        controller: TextEditingController(),
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                            suffixIcon: Icon(Icons.visibility_off_rounded),
+                            label: Text(
+                              "Password",
+                              style: TextStyle(color: Colors.deepPurple),
+                            )),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "Forgot Password",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.deepPurple),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          height: 60,
+                          width: 250,
+                          decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                  colors: [Colors.deepPurple, Colors.purple]),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: const Center(
+                              child: Text(
+                            "Sign in",
+                            style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 150,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Don't have an account?",
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 15),
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                "SIGN UP",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
